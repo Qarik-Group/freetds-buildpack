1b080f5f6c48a08e254caff35eca349749dad308
Dr Nic Williams <drnicwilliams@gmail.com>
2019-06-09 20:27:56 +0930

    all buildpack-packager commands run within project

--- ci/scripts/shipit.sh
+++ ci/scripts/shipit.sh
@@ -16,19 +16,21 @@ VERSION=$(cat ${VERSION_FROM})
 
 go get github.com/cloudfoundry/libbuildpack/packager/buildpack-packager
 
+RELEASE_OUT=$PWD/$RELEASE_OUT
+mkdir -p ${RELEASE_OUT}/artifacts
+echo "v${VERSION}"             > ${RELEASE_OUT}/tag
+echo "${LIBRARY} v${VERSION}"  > ${RELEASE_OUT}/name
+
 pushd $REPO_ROOT
 source .envrc
 
 rm -f *.zip
 buildpack-packager summary
+buildpack-packager summary     > ${RELEASE_OUT}/notes.md
 buildpack-packager build -cached -stack cflinuxfs3
+mv *.zip                         ${RELEASE_OUT}/artifacts
 popd
 
-mkdir -p ${RELEASE_OUT}/artifacts
-echo "v${VERSION}"             > ${RELEASE_OUT}/tag
-echo "${LIBRARY} v${VERSION}"  > ${RELEASE_OUT}/name
-mv ${REPO_ROOT}/*.zip            ${RELEASE_OUT}/artifacts
-buildpack-packager summary     > ${RELEASE_OUT}/notes.md
 
 cat > ${NOTIFICATION_OUT}/message <<EOF
 New ${LIBRARY} v${VERSION} released. <${REPO_URL}/releases/tag/v${VERSION}|Release notes>.
